eda762df3a025d1db58f429923302230f6bd0934
Vivek Raja <vivek.r.raja@gmail.com>
2020-08-09 16:08:59 +0400

    refactor how to code files into its own repo

--- Udemy Angular/recipe-app/src/app/shared/ingredient.model.ts
+++
@@ -1,8 +0,0 @@
-export class Ingredient {
-  constructor(public name: string, public amount: number, public unit: string) {
-    if (unit === undefined) {
-      this.unit = 'units';
-    }
-  }
-
-}
